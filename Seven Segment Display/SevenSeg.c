5f3686eea9378eab97dd6e46cd95e57df5f003f7
ABHISHEK KUMAR K P <abhishekkumarkp11a@gmail.com>
2025-03-10 12:15:15 +0530

    Create SevenSeg.c

---
+++ Seven Segment Display/SevenSeg.c
@@ -0,0 +1,35 @@
+/*
+ * File:   SEVENSEG.c
+ * Author: abhis_czscrzz
+ *
+ * Created on 10 March, 2025, 11:16 AM
+ */
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+
+unsigned char segment[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+void main(void) {
+    TRISB = 0x00;
+    PORTB = segment[0];
+    while(1){
+        for(int i=0;i<10;i++){
+            PORTB=segment[i];
+            __delay_ms(1000);
+        }
+    }
+    return;
+}
