bbb75a9dc92d70ea26ec19ecde61b253e7523f2f
ABHISHEK KUMAR <abhishekkumarkp11a@gmail.com>
2025-06-15 18:26:41 +0530

    Update Code.c

--- External Interrupts/Falling Edge/Code.c
+++ External Interrupts/Falling Edge/Code.c
@@ -1,1 +1,44 @@
 
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+void __interrupt() LED(void)
+{
+    if(INTCONbits.INTF == 1)
+    {
+        PORTBbits.RB7 ^= 1;
+        INTCONbits.INTF = 0;
+    }
+}
+
+void main(void) 
+{ 
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
+    INTCONbits.INTE = 1;
+    OPTION_REGbits.INTEDG = 0; //interrupt at falling edge
+    TRISB7 = 0;
+    while(1)
+    {
+        //IDLE
+    }
+    return;
+}
+
